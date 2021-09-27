a49faf13ce1681384e27759a3332fdbaa22a7976
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-09-27 16:25:40 -0400

    First Refactor to Kotlin - Curso Iniciado

---
+++ app/src/main/java/br/com/diastecnologia/financas_kotlin/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,12 @@
+package br.com.diastecnologia.financas_kotlin.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.diastecnologia.financas_kotlin.R
+
+class ListaTransacoesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+}
