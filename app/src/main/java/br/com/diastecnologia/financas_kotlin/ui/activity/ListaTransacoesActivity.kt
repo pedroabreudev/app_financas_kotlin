7d553ba0f83bc982f796b3e127723b11fa28acdd
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-09-27 16:58:20 -0400

    01 - Implementando o próprio Adapter

--- app/src/main/java/br/com/diastecnologia/financas_kotlin/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/diastecnologia/financas_kotlin/ui/activity/ListaTransacoesActivity.kt
@@ -4,19 +4,18 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import br.com.diastecnologia.financas_kotlin.R
+import br.com.diastecnologia.financas_kotlin.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
+        
         val transacoes = listOf("Comida - R$ 20,50",
             "Economia - R$ 100,00")
 
-        val arrayAdapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1, transacoes)
-
-        lista_transacoes_listview.setAdapter(arrayAdapter)
+        lista_transacoes_listview.setAdapter(ListaTransacoesAdapter(transacoes, this))
 
     }
 }
