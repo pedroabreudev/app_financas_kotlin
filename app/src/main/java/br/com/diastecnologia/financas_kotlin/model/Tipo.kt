57d4bcbbd4686e641e7fc5e9ec0857624ac0b128
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-09-30 15:53:00 -0400

    04 - Sobrecarga de construtores

---
+++ app/src/main/java/br/com/diastecnologia/financas_kotlin/model/Tipo.kt
@@ -0,0 +1,5 @@
+package br.com.diastecnologia.financas_kotlin.model
+
+enum class Tipo {
+    RECEITA, DESPESA,
+}
--- app/src/main/java/br/com/diastecnologia/financas_kotlin/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/diastecnologia/financas_kotlin/ui/activity/ListaTransacoesActivity.kt
@@ -3,6 +3,7 @@ package br.com.diastecnologia.financas_kotlin.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.diastecnologia.financas_kotlin.R
+import br.com.diastecnologia.financas_kotlin.model.Tipo
 import br.com.diastecnologia.financas_kotlin.model.Transacao
 import br.com.diastecnologia.financas_kotlin.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
@@ -14,8 +15,8 @@ class ListaTransacoesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes = listOf(Transacao(BigDecimal(20.5), "Comida", Calendar.getInstance()), Transacao(
-            BigDecimal(100.0),"Economia", Calendar.getInstance()))
+        val transacoes = listOf(Transacao( valor = BigDecimal(20.5), tipo = Tipo.DESPESA),
+            Transacao(valor = BigDecimal(100.0), categoria = "Economia", tipo = Tipo.RECEITA))
 
         lista_transacoes_listview.setAdapter(ListaTransacoesAdapter(transacoes, this))
 
